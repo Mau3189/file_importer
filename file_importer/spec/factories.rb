3ba1234f4b5d23f19cd7b8ebf5d8633a11c975fb
Mauricio Roldan <mauricio.roldan@runawaybit.com>
2015-11-20 23:26:22 -0600

    Add model specs for Record

--- file_importer/spec/factories.rb
+++ file_importer/spec/factories.rb
@@ -2,4 +2,13 @@ FactoryGirl.define do
   factory :datafile do
     filename 'test1'
   end
+
+  factory :record do
+    campaign_name 'my campaign'
+    impressions "5 stars"
+    clicks 300
+    cost 100_00
+    timestamp Time.now
+    datafile { FactoryGirl.build(:datafile) }
+  end
 end
--- file_importer/spec/models/record_spec.rb
+++ file_importer/spec/models/record_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Record do
+  subject(:record) { FactoryGirl.build(:record) }
+
+  it "has a valid Factory" do
+    expect(record).to be_valid
+  end
+
+  # associations
+  it { is_expected.to belong_to(:datafile) }
+  it { is_expected.to respond_to(:datafile) }
+  it { is_expected.to validate_presence_of(:datafile) }
+
+  # columns
+  it { is_expected.to respond_to(:campaign_name) }
+  it { is_expected.to respond_to(:impressions) }
+  it { is_expected.to respond_to(:cost) }
+  it { is_expected.to respond_to(:clicks) }
+  it { is_expected.to respond_to(:timestamp) }
+
 end

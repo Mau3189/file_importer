6224443cd37b52b7011ff364f644c12bfed9dca9
Mauricio Roldan <mauricio.roldan@runawaybit.com>
2015-11-20 21:43:33 -0600

    Add specs for datafile model

--- file_importer/app/models/datafile.rb
+++ file_importer/app/models/datafile.rb
@@ -1,3 +1,4 @@
 class Datafile < ActiveRecord::Base
-
+  validates_uniqueness_of :filename
+  validates_presence_of :filename
 end
--- file_importer/spec/models/datafile_spec.rb
+++ file_importer/spec/models/datafile_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Datafile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Datafile do
+  subject(:datafile) { FactoryGirl.build(:datafile) }
+
+  it "has a valid Factory" do
+    expect(datafile).to be_valid
+  end
+
+  # validations
+  it { is_expected.to validate_uniqueness_of :filename }
+
+  # columns
+  it { is_expected.to respond_to(:filename) }
+  it { is_expected.to validate_presence_of(:filename) }
 end
